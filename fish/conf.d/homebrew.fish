c46106955980e057827e17779888df63a7c70a92
Rishi Sharma <consularparadi@gmail.com>
2025-08-07 12:57:26 +0530

    Updated homebrew env variables

--- fish/conf.d/homebrew.fish
+++ fish/conf.d/homebrew.fish
@@ -1,3 +1,3 @@
-set PATH $HOME/homebrew/bin:$PATH
-set HOMEBREW_CASK_OPTS "--appdir=$HOME/Applications"
-set HOMEBREW_NO_AUTO_UPDATE 1
+set -gx PATH $HOME/homebrew/bin:$PATH
+set -gx HOMEBREW_CASK_OPTS "--appdir=$HOME/Applications"
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
